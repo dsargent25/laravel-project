b58ffd7c22a942a79d84717b53ba50a4f655ef2f
dsargent25 <dsargent@wakeindustrial.com>
2024-08-20 14:59:23 -0400

    Initial Version of the Application with additional changes

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+use Illuminate\Support\Carbon;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('chirps.index',[
+	'chirps' => Chirp::with('user')->latest()->get(),
+	]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+		'message' => 'required|string|max:255',
+	]);
+
+	$request->user()->chirps()->create($validated);
+
+	return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    public function latest(Chirp $chirp): View
+    {
+        $chirp = Carbon::now()->subDays(7);
+
+        return view('chirps.index',[
+            'chirps' => Chirp::with('user')->latest()->get(),
+            ]);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp): View
+    {
+        Gate::authorize('update', $chirp);
+
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp): RedirectResponse
+    {
+        Gate::authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255'
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp): RedirectResponse
+    {
+        Gate::authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\PastWeekDataController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::group(['prefix' => 'chirps'], function () {
+    Route::get('/', [ChirpController::class, 'index'])->name('chirps.index');
+    Route::post('/', [ChirpController::class, 'store'])->name('chirps.store');
+    Route::get('/{chirp}/edit', [ChirpController::class, 'edit'])->name('chirps.edit');
+    Route::put('/{chirp}', [ChirpController::class, 'update'])->name('chirps.update');
+    Route::delete('/{chirp}', [ChirpController::class, 'destroy'])->name('chirps.destroy');
+    Route::get('/latest', [ChirpController::class, 'latest'])->name('chirps.latest');
+});
+
+require __DIR__.'/auth.php';
