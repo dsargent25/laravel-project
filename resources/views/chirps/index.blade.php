58e9b7e2ffab027cc8c23168f670947af9e8df83
dsargent25 <dsargent@wakeindustrial.com>
2024-08-29 13:15:42 -0400

    profile_img_url column added to users table and model and profile image replaces text bubble

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -18,7 +18,8 @@
                     <div class="flex-1">
                         <div class="flex justify-between items-center">
                             <div>
-                                    <span class="text-gray-800">{{ $chirp->user->name }}</span>
+                                <img width="50" height="50" src=" {{$chirp->user->profile_image_url}}">
+                                <span class="text-gray-800">{{ $chirp->user->name }}</span>
                                 <small class="ml-2 text-sm text-gray-600">{{ $chirp->created_at->format('j M Y, g:i a') }}</small>
                             </div>
  				                @unless ($chirp->created_at->eq($chirp->updated_at))
