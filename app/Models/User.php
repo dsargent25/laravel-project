e3d4a4712ffd7dab0b7a3730f23e33037af13bc3
dsargent25 <dsargent@wakeindustrial.com>
2024-09-03 14:22:48 -0400

    Changed HasManyThrough to HasMany for the comment relationship to the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -52,9 +51,9 @@ class User extends Authenticatable
        return $this->hasMany(Chirp::class);
     }
 
-    public function comments(): HasManyThrough
+    public function comments(): HasMany
     {
-     return $this->hasManyThrough(Comment::class, Chirp::class);
+     return $this->hasMany(Comment::class, Chirp::class);
     }
 
 }
