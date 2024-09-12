7b45124294b3ec4d561cf94fb14471cda7e2a114
dsargent25 <dsargent@wakeindustrial.com>
2024-09-12 10:44:55 -0400

    Moved creating chirp container back into index.blade.php. Reverted change for better consistency

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -2,6 +2,21 @@
 
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
 
+        <div class="bg-white rounded-lg shadow-lg z-10" style="padding:1rem;position:fixed;bottom:0;right:0;width:350px;" id="chirpbox">
+            <form method="POST" action="{{ route('chirps.store') }}">
+                @csrf
+                <textarea
+                    name="message"
+                    required
+                    placeholder="{{ __('What\'s on your mind?') }}"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                    style="height:200px;"
+                >{{ old('message') }}</textarea>
+                <x-input-error :messages="$errors->get('message')" class="mt-2" />
+                <x-primary-button class="mt-4" style="background-color:#26A7DE;">{{ __('Chirp') }}</x-primary-button>
+            </form>
+        </div>
+
             @foreach ($chirps as $chirp)
                 <x-large-chirp :chirp="$chirp"/>
             @endforeach
