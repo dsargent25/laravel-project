c8693c6e14b639453f89e8b24b9d08afeffa308d
dsargent25 <dsargent@wakeindustrial.com>
2024-09-12 10:39:07 -0400

    Created 'large-chirp' and 'chirp-comments' components

---
+++ resources/views/components/chirp-comments.blade.php
@@ -0,0 +1,12 @@
+@props(['comment'])
+
+<div>
+    <p class="leading-3 text-sm text-gray-600">{{ $comment->user->name }} chirps: {{ $comment->content }}</p>
+</div>
+@if ($comment->user->is(auth()->user()))
+    <form method="POST" action="{{ route('comments.destroy', $comment) }}">
+    @csrf
+    @method('delete')
+    <button class=" text-red-600 text-xs px-6" :href="route('comments.destroy', $comment)" onclick="event.preventDefault(); this.closest('form').submit();">Delete Comment</button>
+    </form>
+@endif
