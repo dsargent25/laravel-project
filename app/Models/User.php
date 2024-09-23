015153b45cb717e2e533c35a1ec7e60760636746
dsargent25 <dsargent@wakeindustrial.com>
2024-09-23 15:05:57 -0400

    Created images table migration. Created 'Image' model. Established relationships on Image and User models.

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,8 @@ class User extends Authenticatable
      return $this->belongsToMany(User::class,'follower_user', 'user_id', 'follower_id')->withTimestamps();
     }
 
+    public function images(){
+     return $this->hasMany(Image::class);
+    }
+
 }
