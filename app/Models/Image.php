228242501b9524dccb399fb923a9eae971537c22
dsargent25 <dsargent@wakeindustrial.com>
2024-09-27 15:41:17 -0400

    Created migration for user_images table. Made user_id and image_id fillable. Image hasMany UserImages, UserImage belongsTo Image, and User hasManyThrough UserImages through Images.

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Image extends Model
 {
@@ -19,6 +20,11 @@ class Image extends Model
     public function user(): BelongsTo
     {
      return $this->belongsTo(User::class);
+    }
+
+    public function userImages(): HasMany
+    {
+     return $this->hasMany(UserImage::class);
     }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -66,6 +67,11 @@ class User extends Authenticatable
 
     public function images(){
      return $this->hasMany(Image::class);
+    }
+
+    public function userImages(): HasManyThrough
+    {
+     return $this->hasManyThrough(UserImage::class, Image::class);
     }
 
 }
